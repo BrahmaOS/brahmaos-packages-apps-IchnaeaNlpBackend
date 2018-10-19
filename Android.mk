5a70cf8032d4d654ebcefaa981dc960c904d2867
winton.liu <admin@brahmaos.io>
2018-10-19 10:56:11 +0800

    Fix crash issue when entering mozilla market
    Remove mozilla market link
    
    Change-Id: Iaa0bb11b626a42728ef4906ea652f47c9b6ab8eb
    Signed-off-by: winton.liu <admin@brahmaos.io>

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := IchnaeaNlpBackend
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
+LOCAL_DEX_PROPT := false
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
